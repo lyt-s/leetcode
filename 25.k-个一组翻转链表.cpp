201512e236b74e42705381f6e60b2c333251c5fc
lyt-s <1814666859@qq.com>
2024-02-21 13:45:11 +0800

    Changes to be committed: 	new file:   "25.k-\344\270\252\344\270\200\347\273\204\347\277\273\350\275\254\351\223\276\350\241\250.cpp"
    Changes not staged for commit:
    	modified:   "160.\347\233\270\344\272\244\351\223\276\350\241\250.cpp"

---
+++ 25.k-个一组翻转链表.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=cpp
+ *
+ * [25] K 个一组翻转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        // 链表是否为空或k是否为1
+        if(head == nullptr || head->next == nullptr){
+            return head;
+        } 
+        int count = 0;
+        for (ListNode *cur = head; cur; cur = cur->next){
+            ++count; // 统计节点个数
+        }
+        ListNode *dummy = new ListNode(0);
+        ListNode *prev = new ListNode(0);
+        dummy = prev;
+        prev->next = nullptr;
+        ListNode *curr = head , *temp;
+         ListNode* next_start = curr;  // 下一次 k循环启动的节点
+
+        while(count >= k){
+            for(int i = 0; i < k; i++){
+                temp = curr->next;
+                curr->next = prev->next;
+                prev->next = curr;
+                curr = temp;
+            }
+            prev = next_start;
+            next_start->next = curr;
+            next_start = curr;
+            count-=k;
+        }
+        return dummy->next;
+    }
+};
+// @lc code=end
+
