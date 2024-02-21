09a193e8c39013ceb973b36c69abf1a7864bdb71
lyt-s <1814666859@qq.com>
2024-02-21 13:45:23 +0800

    modified:   "160.\347\233\270\344\272\244\351\223\276\350\241\250.cpp"

--- 160.相交链表.cpp
+++ 160.相交链表.cpp
@@ -15,40 +15,58 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* curA = headA;
-        ListNode* curB = headB;
-        int lenA = 0,lenB =0;
-        while(curA){
-            curA = curA->next;
-            lenA++;
-        }
-        while(curB){
-            curB = curB->next;
-            lenB++;
-        }
-        curA = headA;
-        curB = headB;
-        // 让curA为最长链表的头，lenA为其长度
-        if (lenB > lenA) {
-            swap (lenA, lenB);
-            swap (curA, curB);
-        }
+    // ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    //     ListNode* curA = headA;
+    //     ListNode* curB = headB;
+    //     int lenA = 0,lenB =0;
+    //     while(curA){
+    //         curA = curA->next;
+    //         lenA++;
+    //     }
+    //     while(curB){
+    //         curB = curB->next;
+    //         lenB++;
+    //     }
+    //     curA = headA;
+    //     curB = headB;
+    //     // 让curA为最长链表的头，lenA为其长度
+    //     if (lenB > lenA) {
+    //         swap (lenA, lenB);
+    //         swap (curA, curB);
+    //     }
 
-        // 求长度差
-        int gap = lenA - lenB;
-        // 让curA和curB在同一起点上（末尾位置对齐）
-        while (gap--) {
-            curA = curA->next;
+    //     // 求长度差
+    //     int gap = lenA - lenB;
+    //     // 让curA和curB在同一起点上（末尾位置对齐）
+    //     while (gap--) {
+    //         curA = curA->next;
+    //     }
+    //     while(curA){
+    //         if(curA == curB){
+    //             return curA;
+    //         }
+    //         curA = curA->next;
+    //         curB = curB->next;
+    //     }
+    //     return NULL;
+    // }
+
+    // 1. 初始化两个指针 pA 和 pB，分别指向链表 headA 和 headB 的头节点。
+    // 2. 同时遍历两个链表，当指针 pA和 pB都到达末尾时，它们走过的路径长度相等。如果链表相交，它们会在相交节点相遇；如果链表不相交，它们会在末尾节点null相遇。
+    //      遍历过程中，如果 pA 到达链表末尾，将其重置为链表 headB 的头节点；如果 pB 到达链表末尾，将其重置为链表 headA 的头节点。
+    //      这样，当 pA 和 pB 再次到达链表末尾时，它们走过的节点数相等，且它们所经过的末尾部分节点完全相同。因此，如果链表相交，它们会在相交节点相遇；如果链表不相交，它们会在末尾节点（null）相遇。
+    // 3. 返回相交节点或 null。
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *a = headA;
+        ListNode *b = headB;
+        while(a != nullptr || b != nullptr) {
+            if(a == nullptr ) a = headB;
+            if(b == nullptr) b = headA;
+            if(a == b) return a;
+            a = a->next;
+            b = b->next;
         }
-        while(curA){
-            if(curA == curB){
-                return curA;
-            }
-            curA = curA->next;
-            curB = curB->next;
-        }
-        return NULL;
+        return nullptr;
     }
 };
 // @lc code=end
