ac9f123a7bc2ade012be2b8c5405990d344d1b25
lyt-s <1814666859@qq.com>
2024-03-07 22:59:50 +0800

    modified:   "146.lru-\347\274\223\345\255\230.cpp"

--- 146.lru-缓存.cpp
+++ 146.lru-缓存.cpp
@@ -1,84 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=146 lang=cpp
+ *
+ * [146] LRU 缓存
+ */
+
+// @lc code=start
 class LRUCache {
-private:
+    // 进出的顺序
+    std::list<std::pair<int,int>> m_list;
+    std::map<int,decltype(m_list.begin())> m_map;
+    int m_capacity;
 
-    list<pair<int,int>> list_;
-    map<int,decltype(list_.begin())> mp;
-    int capacity_;
-
-    // 得到最近
-    void makeRecently(int key){
-        auto temp_it = list_.insert(list_.end(),*mp[key]);
-        list_.erase(mp[key]);
-        mp[key] = temp_it;
+    // 1.get page
+    void getRecentlyPage(int key){
+        auto it = m_map.find(key);
+        // it.second == m_map[key];
+        if(it != m_map.end()){
+            // auto temp_it = m_list.insert(m_list.end(),std::make_pair(key,it->second->second));
+            auto temp_it = m_list.insert(m_list.end(),it->second);
+            m_list.erase(it->second);
+            m_map[key] = temp_it; 
+        }
     }
 
-    void addPoint(int key ,int value){
-        auto temp_it = list_.insert(list_.end(),{key,value});
-        mp.emplace(std::make_pair(key,temp_it));
+    //2. delete 
+    void deleteOldestPage(){
+        m_map.erase(m_list.begin()->first);
+        m_list.pop_front();
     }
 
-    void deltetOldest(){
-        mp.erase(list_.begin()->first);
-        list_.pop_front();
+    //3. update
+    void update(int key , int value){
+        m_map[key]->second = value;
+        getRecentlyPage(key);
     }
 
-    void updateValue(int key,int value){
-        mp[key]->second = value;
-        makeRecently(key);
+    // 4. addNewPage
+    void addNewPage(int key, int value){
+        auto it = m_list.insert(m_list.end(),std::make_pair(key,value));
+        m_map[key] = it;
     }
 public:
-
-    LRUCache(int capacity) :capacity_(capacity) {
+    LRUCache(int capacity) : m_capacity(capacity){
 
     }
     
     int get(int key) {
-        if(mp.find(key) != mp.end()){
-            makeRecently(key);
-            return mp[key]->second;
+        auto it = m_map.find(key);
+        if(it != m_map.end()){
+            getRecentlyPage(key);
+            return it->second->second;
         }
         return -1;
     }
     
     void put(int key, int value) {
-        if(mp.find(key)==mp.end()){ // 没找到
-            if(list_.size() == capacity_){ // 满了
-                deltetOldest(); // 删掉最老节点
+        if(m_map.find(key) == m_map.end()){
+            if(m_list.size() == m_capacity){
+                deleteOldestPage();
             }
-            addPoint(key, value); // 添加新节点
-        }else{ // 找到了
-            updateValue(key, value); // 更新节点值
+            addNewPage(key, value);
+        }else{
+            update(key, value);
         }
-
     }
 };
 
-class LRUCache {
-private:
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+// @lc code=end
 
-    list<pair<int,int>> m_list;  // [key,value]
-    map<int,decltype<m_list.begin()> m_map; // [key, iterator]
-    int m_capacity;
-
-    void update(int key, int value){
-        
-    }
-
-    void addNewPoint(int key, int value){
-
-    }
-public:
-
-    LRUCache(int capacity) :m_capacity(capacity) {
-
-    }
-    
-    int get(int key) {
-
-    }
-    
-    void put(int key, int value) {
-
-
-    }
-};
